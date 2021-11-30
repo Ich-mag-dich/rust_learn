ae123e048b2538c92f8184460ac6bf18b74a380e
Ich-mag-dich <san02@kakao.com>
2021-12-01 03:45:08 +0900

    update

--- test/main.rs
+++ test/main.rs
@@ -3,11 +3,13 @@ fn my_num() {
     let my_numbers = 10;
     println!("{}", my_numbers);
     {
+        println!("{}", my_numbers);
         let my_numbers = 20;
         println!("{}",my_numbers);
     }
     println!("{}", my_numbers);
 }
+
 
 fn main() {
     my_num();
