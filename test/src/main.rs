dedc2d58c400cd645aeed5e11fbccac865632bb7
Ich-mag-dich <san02@kakao.com>
2021-12-04 00:24:06 +0900

    test

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,13 +1,14 @@
-
 fn my_num() {
-    let my_numbers:i16 = 1_0________________________________;
-    println!("{}", my_numbers);  // 10
+    let my_numbers: i16 = 1_0________________________________;
+    println!("{}", my_numbers); // 10
     {
         println!("{}", my_numbers); // 10
-        let my_numbers:i16 = 20;
-        println!("{}",my_numbers); // 20
+        let my_numbers: i16 = 20;
+        println!("{}", my_numbers); // 20
     }
     println!("{}", my_numbers); // 10
+
+    println!("test");
 }
 
 fn main() {
